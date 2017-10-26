9d3e9b8e7089c3f00a46bea74273c1d59c905699
KyleLeeners <kyle.jleeners@gmail.com>
2017-10-25 19:05:28 -0700

    Added file to squash csv's ~50%

---
+++ SquashTxtFile.py
@@ -0,0 +1,21 @@
+# use this to make your midi.txt files a bit smaller (easier to input into the RNN)
+
+import os.path
+import re
+
+directory = r'C:\Users\Kyle\Desktop\pythonProjects\ClassicalGuitarEmulator\csvFiles\\'
+rep = {"Note_on_c": "c", "Note_off_c": "f", " ": "", "Control_c": "cc", "Program_c": "p"}
+
+for file in os.listdir(directory):
+    with open(directory + file, 'r') as f:
+        newLine = ""
+        for word in f.readlines():
+            newLine += word
+
+    rep = dict((re.escape(k), v) for k, v in rep.items())
+    pattern = re.compile("|".join(rep.keys()))
+    newLine = pattern.sub(lambda m: rep[re.escape(m.group(0))], newLine)
+
+    with open(directory + file, "w") as f:
+        for line in newLine:
+            f.writelines(line)
