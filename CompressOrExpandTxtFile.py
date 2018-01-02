c3badf17c055bc97b2b49975d034c1b0efa0f74f
KyleLeeners <kyle.jleeners@gmail.com>
2018-01-02 14:38:47 -0800

    Added LSTM

--- CompressOrExpandTxtFile.py
+++ CompressOrExpandTxtFile.py
@@ -4,9 +4,9 @@ import os.path
 import re
 
 directory = r'C:\Users\Kyle\Desktop\pythonProjects\ClassicalGuitarEmulator\csvFiles\\'
-rep = {"Note_on_c": "c", "Note_off_c": "f",
-       " ": "", "Control_c": "cc",
-       "Program_c": "p", "Pitch_bend_c": "pb"}
+rep = {"Note_on_c": "c", "Note_off_c": "f", " ": "", "Control_c": "cc", "Program_c": "p", "Pitch_bend_c": "pb"}
+rep = dict((re.escape(k), v) for k, v in rep.items())
+pattern = re.compile("|".join(rep.keys()))
 
 # use this to convert back!
 # rep = {",c": ", Note_on_c", ",f": ", Note_off_c",
@@ -14,13 +14,11 @@ rep = {"Note_on_c": "c", "Note_off_c": "f",
 #        ",p": ", Program_c", ",pb": ", Pitch_bend_c"}
 
 for file in os.listdir(directory):
+    newLine = ""
     with open(directory + file, 'r') as f:
-        newLine = ""
         for word in f.readlines():
             newLine += word
 
-    rep = dict((re.escape(k), v) for k, v in rep.items())
-    pattern = re.compile("|".join(rep.keys()))
     newLine = pattern.sub(lambda m: rep[re.escape(m.group(0))], newLine)
 
     with open(directory + file, "w") as f:
