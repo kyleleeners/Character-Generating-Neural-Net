1da9e0416bbccb53d7d469921f864fa36d40e3e1
KyleLeeners <kyle.jleeners@gmail.com>
2018-05-04 19:21:13 -0700

    built my own lstm

--- LSTM.py
+++ LSTM.py
@@ -1,116 +1,82 @@
-"""
-Minimal character-level Vanilla RNN model. Written by Andrej Karpathy (@karpathy)
-BSD License
-"""
+import argparse
+import os
 import numpy as np
-
-# data I/O
-data = open('input.txt', 'r').read()  # should be simple plain text file
-chars = list(set(data))
-data_size, vocab_size = len(data), len(chars)
-print('data has %d characters, %d unique.' % (data_size, vocab_size))
-char_to_ix = {ch: i for i, ch in enumerate(chars)}
-ix_to_char = {i: ch for i, ch in enumerate(chars)}
-
-# hyperparameters
-hidden_size = 200  # size of hidden layer of neurons
-seq_length = 50 # number of steps to unroll the RNN for
-learning_rate = 1e-1
-
-# model parameters
-Wxh = np.random.randn(hidden_size, vocab_size) * 0.01  # input to hidden
-Whh = np.random.randn(hidden_size, hidden_size) * 0.01  # hidden to hidden
-Why = np.random.randn(vocab_size, hidden_size) * 0.01  # hidden to output
-bh = np.zeros((hidden_size, 1))  # hidden bias
-by = np.zeros((vocab_size, 1))  # output bias
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
 
 
-def lossFun(inputs, targets, hprev):
-    """
-  inputs,targets are both list of integers.
-  hprev is Hx1 array of initial hidden state
-  returns the loss, gradients on model parameters, and last hidden state
-  """
-    xs, hs, ys, ps = {}, {}, {}, {}
-    hs[-1] = np.copy(hprev)
-    loss = 0
-    # forward pass
-    for t in range(len(inputs)):
-        xs[t] = np.zeros((vocab_size, 1))  # encode in 1-of-k representation
-        xs[t][inputs[t]] = 1
-        hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t - 1]) + bh)  # hidden state
-        ys[t] = np.dot(Why, hs[t]) + by  # unnormalized log probabilities for next chars
-        ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t]))  # probabilities for next chars
-        loss += -np.log(ps[t][targets[t], 0])  # softmax (cross-entropy loss)
-    # backward pass: compute gradients going backwards
-    dWxh, dWhh, dWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-    dbh, dby = np.zeros_like(bh), np.zeros_like(by)
-    dhnext = np.zeros_like(hs[0])
-    for t in reversed(range(len(inputs))):
-        dy = np.copy(ps[t])
-        dy[targets[
-            t]] -= 1  # backprop into y. see http://cs231n.github.io/neural-networks-case-study/#grad if confused here
-        dWhy += np.dot(dy, hs[t].T)
-        dby += dy
-        dh = np.dot(Why.T, dy) + dhnext  # backprop into h
-        dhraw = (1 - hs[t] * hs[t]) * dh  # backprop through tanh nonlinearity
-        dbh += dhraw
-        dWxh += np.dot(dhraw, xs[t].T)
-        dWhh += np.dot(dhraw, hs[t - 1].T)
-        dhnext = np.dot(Whh.T, dhraw)
-    for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
-        np.clip(dparam, -5, 5, out=dparam)  # clip to mitigate exploding gradients
-    return loss, dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs) - 1]
+def load_data(filename):
+    with open(os.path.join('..', 'data', filename), 'r') as f:
+        return f.read()
 
 
-def sample(h, seed_ix, n):
-    """
-  sample a sequence of integers from the model
-  h is memory state, seed_ix is seed letter for first time step
-  """
-    x = np.zeros((vocab_size, 1))
-    x[seed_ix] = 1
-    ixes = []
-    for t in range(n):
-        h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
-        y = np.dot(Why, h) + by
-        p = np.exp(y) / np.sum(np.exp(y))
-        ix = np.random.choice(range(vocab_size), p=p.ravel())
-        x = np.zeros((vocab_size, 1))
-        x[ix] = 1
-        ixes.append(ix)
-    return ixes
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-tr', '--training', required=True)
 
+    io_args = parser.parse_args()
+    training = io_args.training
 
-n, p = 0, 0
-mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-mbh, mby = np.zeros_like(bh), np.zeros_like(by)  # memory variables for Adagrad
-smooth_loss = -np.log(1.0 / vocab_size) * seq_length  # loss at iteration 0
-while True:
-    # prepare inputs (we're sweeping from left to right in steps seq_length long)
-    if p + seq_length + 1 >= len(data) or n == 0:
-        hprev = np.zeros((hidden_size, 1))  # reset RNN memory
-        p = 0  # go from start of data
-    inputs = [char_to_ix[ch] for ch in data[p:p + seq_length]]
-    targets = [char_to_ix[ch] for ch in data[p + 1:p + seq_length + 1]]
+    # setup data
+    training_data = load_data(training).lower()
+    chars = list(set(training_data))
+    data_size, alphabet_size = len(training_data), len(chars)
+    print('%d characters, %d unique' % (data_size, alphabet_size))
+    char_to_ix = {ch: i for i, ch in enumerate(chars)}
+    ix_to_char = {i: ch for i, ch in enumerate(chars)}
+    seq_length = 100
+    raw_X = []
+    raw_y = []
+    for i in range(0, data_size - seq_length, 1):
+        seq_X = training_data[i:i+seq_length]
+        seq_y = training_data[i+seq_length]
+        raw_X.append([char_to_ix[c] for c in seq_X])
+        raw_y.append(char_to_ix[seq_y])
 
-    # sample from the model now and then
-    if n % 100 == 0:
-        sample_ix = sample(hprev, inputs[0], 200)
-        txt = ''.join(ix_to_char[ix] for ix in sample_ix)
-        print('----\n %s \n----' % (txt,))
+    # standardize
+    n_patterns = len(raw_X)
+    X = np.reshape(raw_X, (n_patterns, seq_length, 1))
+    X = X / float(alphabet_size)
+    y = np_utils.to_categorical(raw_y)
 
-    # forward seq_length characters through the net and fetch gradient
-    loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
-    smooth_loss = smooth_loss * 0.999 + loss * 0.001
-    if n % 100 == 0: print('iter %d, loss: %f' % (n, smooth_loss))  # print progress
+    # setup model
+    model = Sequential()
+    model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(256))
+    model.add(Dropout(0.2))
+    model.add(Dense(y.shape[1], activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    # perform parameter update with Adagrad
-    for param, dparam, mem in zip([Wxh, Whh, Why, bh, by],
-                                  [dWxh, dWhh, dWhy, dbh, dby],
-                                  [mWxh, mWhh, mWhy, mbh, mby]):
-        mem += dparam * dparam
-        param += -learning_rate * dparam / np.sqrt(mem + 1e-8)  # adagrad update
+    # find best weights all epocs
+    filename = "weights.best.hdf5"
+    checkpoint = ModelCheckpoint(filename, monitor='loss', verbose=1, save_best_only=True, mode='min')
 
-    p += seq_length  # move data pointer
-    n += 1  # iteration counter
+    callbacks_list = [checkpoint]
+
+    # fit the model
+    model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+
+    # load best
+    model.load_weights(filename)
+
+    # pick a random seed
+    start = np.random.randint(0, len(raw_X) - 1)
+    pattern = raw_X[start]
+
+    # generate characters
+    result = ''
+    for i in range(1000):
+        x = np.reshape(pattern, (1, len(pattern), 1))
+        x = x / float(alphabet_size)
+        prediction = model.predict(x, verbose=0)
+        index = np.argmax(prediction)
+        result += ix_to_char[index]
+        seq_in = [ix_to_char[value] for value in pattern]
+        pattern.append(index)
+        pattern = pattern[1:len(pattern)]
+    print(result)
